8023b3f3ea9bb4cd38e1e7d6adf10ee92e162a66
alex <alex.basin89@gmail.com>
2025-01-14 22:12:23 +0100

    Initial commit

--- src/main/java/vision/psy/flacdump/Application.java
+++ src/main/java/vision/psy/flacdump/Application.java
@@ -12,9 +12,5 @@ public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-
-		// nur zum Testen:
-		// UserAccountService.login("ooz","pass13");
 	}
-
 }
